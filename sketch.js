899cb3725e0a71770daae1eb00587dc7bee20df6
Christopher Swift <weareswift@gmail.com>
2021-01-03 14:19:36 -0500

    Uploading p5.js and txt files from my desktop
    I am making an Instagram bot and want to run it from hosted space.

---
+++ sketch.js
@@ -0,0 +1,57 @@
+let img;
+let myText;
+let randomLineOfText;
+let writer;
+
+function preload() {
+  myText = loadStrings('allWords.txt');
+}
+
+function setup() {
+  background(220);
+  createCanvas(420, 420);
+  randomLineOfText = round(random(0, myText.length));
+  console.log(myText[randomLineOfText]);
+  // console.log(myText.join(' '));
+  // console.log(myText);
+  show();
+}
+
+function generate() {
+  const model = new rw.HostedModel({
+    url: "https://attngan-swift.hosted-models.runwayml.cloud/v1/",
+    token: "hvQ16i5t5LM7JrM1AGf8+Q==",
+  });
+  //// You can use the info() method to see what type of input object the model expects
+  // model.info().then(info => console.log(info));
+
+
+  const inputs = {
+    "caption": myText[randomLineOfText],
+
+  };
+  model.query(inputs).then(outputs => {
+    const {
+      result
+    } = outputs;
+    img = createImg(result, myText[randomLineOfText]);
+    img.hide();
+  });
+
+}
+
+function draw() {
+  if (img) {
+    imageMode(CENTER);
+    image(img, width / 2, height / 2, width, height);
+    stroke(12);
+  }
+}
+
+function show() {
+  generate();
+  saveFrames('art-bot', 'jpg', 1, 1);
+  writer = createWriter('art-bot.txt');
+  writer.write(myText[randomLineOfText]);
+  writer.close();
+}
